cc5cb7b269fbf2e10b84ce338c8b9dac4bbf2db7
Hayoung <76105226+gkdud3579@users.noreply.github.com>
2025-03-06 14:21:58 +0900

    Design: NotFound Page Design
    異常なアクセス時、not found page表示

--- kangrumap/src/pages/NotFound.jsx
+++ kangrumap/src/pages/NotFound.jsx
@@ -1,7 +1,11 @@
+import styles from '../styles/NotFound.module.scss';
+
 const NotFound = () => {
     return (
-      <div>
+      <div className={styles.notFound}>
         <h1>404</h1>
+        <p className={styles.description}>PageNotFound</p>
+        <p className={styles.emoji}>🤯</p>
       </div>
     );
   };
